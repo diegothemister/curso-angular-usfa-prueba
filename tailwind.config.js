aed188326282f61d8f51da8e36feb09c78e8dfd6
diegothemister <152827283+diegothemister@users.noreply.github.com>
2024-12-15 00:06:23 -0400

    tema aqua

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {},
   },
   daisyui: {
-    themes: ["light", "dark", "cupcake"],
+    themes: ["light", "dark", "aqua"],
   },
   plugins: [
     require('daisyui'),
