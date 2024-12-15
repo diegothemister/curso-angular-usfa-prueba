11b6141f0679ba051c52cff7b95915240147f623
diegothemister <152827283+diegothemister@users.noreply.github.com>
2024-12-15 00:42:51 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {},
   },
   daisyui: {
-    themes: ["light", "dark", "aqua"],
+    themes: ["synthwave", "dark", "light"],
   },
   plugins: [
     require('daisyui'),
